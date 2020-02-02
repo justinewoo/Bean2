0f6969551fa44ec7400e76e3ee075b2afb650359
Justine Woo <justinewoo8@gmail.com>
2020-02-01 17:04:13 -0800

    second commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,42 @@
 import React, { Component } from "react";
 import './App.css';
+import axios from "axios"
+
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {username: '', password: ''};
+
+    this.handleChange = this.handleChange.bind(this);
+
+  }
+
+  handleChange(event, key) {
+    this.setState({[key]: event.target.value})
+  }
+
+
+  _postUser = () => {
+    var self = this;
+    const getUsersData = {
+      username: self.state.username,
+      password: self.state.password 
+
+    }
+
+    
+
+    const querystring = require('querystring');
+    console.log(querystring.stringify(getUsersData))
+    axios.post("Access-Control-Allow-Origin:http://45.79.228.167:8000/accounts?", querystring.stringify(getUsersData))
+      .then(function (response){
+        alert('Successfully created account')
+      })
+
+
+  }
+
   render() {
     return (
 
@@ -46,6 +81,8 @@ class App extends Component {
        			height: 20
        			}}
       			placeholder = "Username" 
+            value = {this.state.username}
+            onChange={event => this.handleChange(event, 'username')}
       		/>
       	
       	</div>
@@ -65,6 +102,8 @@ class App extends Component {
       			}}
       			type = "password"
       			placeholder = "Password" 
+            value = {this.state.password}
+            onChange={event => this.handleChange(event, 'password')}
       		/>
       	
       	</div>
@@ -80,7 +119,8 @@ class App extends Component {
         	style = {{
         		width: 250
         		}}
-        	onClick={() => alert("Logged In")}>
+        	onClick={() => {this._postUser()}}
+          >
         	Login</button>
 
         </div>
